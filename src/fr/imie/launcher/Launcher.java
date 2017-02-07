cb802afd237663e3eac2a8082ea8560898300068
Wylliam <wylliam.delaunay@gmail.com>
2017-02-07 11:33:59 +0100

    add class JdbcConnect

--- src/fr/imie/launcher/Launcher.java
+++ src/fr/imie/launcher/Launcher.java
@@ -1,9 +1,28 @@
 package fr.imie.launcher;
 
+import java.sql.DatabaseMetaData;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import fr.imie.jdbc.JdbcConnect;
+
 public class Launcher {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		try {
+		      //TEST DE CONNEXION
+		      PreparedStatement prepare = JdbcConnect.getInstance().prepareStatement("SELECT * FROM classe WHERE cls_nom = ?");
+
+		      Statement state = JdbcConnect.getInstance().createStatement();
+
+		      JdbcConnect.getInstance().setAutoCommit(false);
+
+		      DatabaseMetaData meta = JdbcConnect.getInstance().getMetaData();
+		         
+		    } catch (SQLException e) {
+		      e.printStackTrace();
+		    }
 
 	}
 
